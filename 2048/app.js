2c50fc9d60c792423b9e7ed0e3ad8c22947d6142
Quentin Morineau <q.morineau@gmail.com>
2023-11-08 18:58:26 +0100

    commit

--- 2048/app.js
+++ 2048/app.js
@@ -1,88 +1,141 @@
 // Variables 
 
 const grid = document.querySelector("#container");
+
+let emptyGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 const rows = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16],
 ];
-const rowsReverse = rows.forEach(element => {
-    element.reverse()
+
+const rowsReverse = rows.map(subArray => [...subArray]);
+rowsReverse.forEach(element => {
+   element.reverse()
 });
+
 const columns = [
     [1, 5, 9, 13],
     [2, 6, 10, 14],
     [3, 7, 11, 15],
     [4, 8, 12, 16],
 ];
-const columnsReverse = columns.forEach(element => {
-    element.reverse()
+
+const columnsReverse = columns.map(subArray => [...subArray]);
+columnsReverse.forEach(element => {
+   element.reverse()
 });
 
 // Functions
 
 let randomPosition = () => {
-    random = Math.ceil(Math.random()*4)
+    random = Math.floor(Math.random()*16)
     return random
 }
 
-let moveUp = () => {
-    console.log("test")
-}
-
-let moveDown = () => {
-    console.log("test")
-}
-
-let moveLeft = () => {
-    console.log("test")
-}
-
-let moveRight = () => {
-    console.log("test")
-}
-
-
 let newNumber = () => {
-    let newDiv = document.createElement("div");
-    let newCase = grid.appendChild(newDiv);
-    newCase.textContent = "2";
-    newCase.classList.add("x2");
-    newCase.classList.add("number");
-    newCase.style.gridColumn = randomPosition();
-    newCase.style.gridRow = randomPosition();
+    let cell = grid.children[randomPosition()]
+    while (cell.textContent) {
+        cell = grid.children[randomPosition()]
+    }
+    let random = Math.ceil(Math.random()*5)
+    if (random == 1) {
+        cell.textContent = Number(4)
+        cell.classList.add ("x4")
+    } else {
+        cell.textContent = Number(2)
+        cell.classList.add ("x2")
+    }
+}
+
+let action = (array) => {
+    let temp = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ];
+    for (let i = 0; i < 16; i++) {
+        if (grid.children[i].textContent){
+            emptyGrid[i] = grid.children[i].textContent
+        }
+    }
+    for (let i = 0; i < 16; i++) {
+        if (emptyGrid[i]) {
+            for (let j = 0; j < 4; j++) {
+                if (array[j].indexOf(i+1) !== -1) {
+                    temp[j][array[j].indexOf(i+1)] = emptyGrid[i]
+                }
+            }
+        }
+    }
+    let newTemp = []
+    temp.forEach(element => {
+        let filtered = element.filter((number) => number != 0)
+        while (filtered.length !== 4) {
+            filtered.unshift(0)
+        }
+        newTemp.push(filtered)
+    })
+    resetGrid()
+    removeNumber()
+    for (let i = 0; i <= 3; i++) {
+        for (let j = 2; j >= 0; j--) {
+            if (newTemp[i][j] == newTemp[i][j+1] && newTemp[i][j] !== 0) {
+                newTemp[i][j+1] = parseInt(newTemp[i][j])+parseInt(newTemp[i][j+1])
+                newTemp[i].splice(j, 1)
+                if (newTemp[i].length !== 4) {
+                    newTemp[i].unshift(0)
+                }
+            }
+        }
+    }
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            if (newTemp[i][j]) {
+                grid.children[array[i][j]-1].textContent = newTemp[i][j]
+                grid.children[array[i][j]-1].classList.add(`x${grid.children[array[i][j]-1].textContent}`)
+            }
+        }
+    }
+    
+    resetGrid()
+}
+
+let removeNumber = () => {
+    for (i = 0; i < 16; i++) {
+        grid.children[i].textContent = "";
+        grid.children[i].removeAttribute("class");
+        grid.children[i].classList.add("block");
+    }
+}
+
+let resetGrid = () => {
+    emptyGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
 
 // Actions
 
 newNumber()
 document.addEventListener("keyup", (e) => {
-    let numbers = document.querySelectorAll(".number")
     switch(e.key) {
         case "ArrowUp" :
-            numbers.forEach(element => {
-                moveUp(element)
-            });
-            console.log(gridElement)
-            resetGrid()
+            action(columnsReverse)
+            newNumber()
             break;
         case "ArrowDown" :
-            numbers.forEach(element => {
-                moveDown(element)
-            });
+            action(columns)
+            newNumber()
             break;
         case "ArrowLeft" :
-            numbers.forEach(element => {
-                moveLeft(element)
-            });
+            action(rowsReverse)
+            newNumber()
             break;
         case "ArrowRight" :
-            numbers.forEach(element => {
-                moveRight(element)
-            });
+            action(rows)
+            newNumber()
             break;
-
     }
-    newNumber()
 })
